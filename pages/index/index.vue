b98cb07b4b072d13d9ba10977e09ed5b2598f7cf
叶凤 <feng.ye@qcast.cn>
2020-04-26 10:09:19 +0800

    add basic data

---
+++ pages/index/index.vue
@@ -0,0 +1,28 @@
+<template>
+	<view class="content">
+		<image src="../../static/g1.gif" mode="widthFix"></image>
+		<text class="title">小姐姐，做我女朋友吧</text>
+		<view class="operate">
+			
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="less">
+	@import url("index.less");
+</style>
